c7e10cc0333f26b251929d6e0decfa095976be1b
Red711 <redouane.guechoud@gmail.com>
2021-02-28 23:08:02 +0100

    Updates

---
+++ precache-manifest.7db52ae32622fbadb2eb1c28d65790d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75f8e23450826fa71d19ce4897c7b64",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "f18e2880874dad1c8bba",
+    "url": "/redux/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "abdc31d1c955a87c462e",
+    "url": "/redux/static/js/2.0baf262b.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/redux/static/js/2.0baf262b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18e2880874dad1c8bba",
+    "url": "/redux/static/js/main.77d7450c.chunk.js"
+  },
+  {
+    "revision": "184cdfdc75337c01b4e3",
+    "url": "/redux/static/js/runtime-main.f2828f9d.js"
+  }
+]);
